c540929cc40030a8be9fff2444dec67445948f5b
Michael  Smith <MichaelSmith@Michaels-MacBook-Air-2.local>
2021-10-01 21:31:47 +0100

    added variables, cloud provider and module

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone = var.zone
+}
+
+
+module "instances" {
+  source = "./modules/instances"
+}
