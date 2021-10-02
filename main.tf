c852f1afb977d2c224eaab153a23b801b8e4c7b3
Michael  Smith <MichaelSmith@Michaels-MacBook-Air-2.local>
2021-10-02 15:45:33 +0100

    added vpc network module from registry

--- main.tf
+++ main.tf
@@ -16,6 +16,31 @@ provider "google" {
   zone = var.zone
 }
 
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 3.2.2"
+
+    project_id   = var.project_id
+    network_name = "terraform-vpc"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+        {
+            subnet_name           = "subnet-01"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = "us-central1"
+        },
+        {
+            subnet_name           = "subnet-02"
+            subnet_ip             = "10.10.20.0/24"
+            subnet_region         = "us-central1"
+            subnet_private_access = "true"
+            subnet_flow_logs      = "true"
+            description           = "This subnet has a description"
+        }
+    ]
+}
+
 
 module "instances" {
   source = "./modules/instances"
